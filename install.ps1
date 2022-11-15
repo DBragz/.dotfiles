6116623eb19a817a2c008ef0bb1bc56e1738b51b
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2022-11-14 22:58:49 -0700

    Create install.ps1
    Created initial start script for Windows environment.

---
+++ install.ps1
@@ -0,0 +1,16 @@
+<# .SYNOPSIS
+	Windows Environment Initializa 
+.DESCRIPTION
+	Initial installation script for my Windows environment.
+.NOTES
+	Author: Daniel Ribeirinha-Braga - dmrbraga@gmail.com
+#>
+
+Invoke-WebRequest -useb chocolatey.org/install.ps1 | Invoke-Expression
+
+Invoke-WebRequest -useb get.scoop.sh | Invoke-Expression
+
+scoop bucket add main
+scoop bucket add extras
+
+scoop install winget
