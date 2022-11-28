dec540269b9d454444c63cfa24f11e704079d977
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2022-11-27 23:18:09 -0500

    Added neovim installation and updating config.

--- install.ps1
+++ install.ps1
@@ -1,5 +1,5 @@
 <# .SYNOPSIS
-	Windows Environment Initializa 
+	Windows Environment Initialization
 .DESCRIPTION
 	Initial installation script for my Windows environment.
 .NOTES
@@ -12,3 +12,8 @@ Invoke-WebRequest -useb get.scoop.sh | Invoke-Expression
 
 scoop bucket add main
 scoop bucket add extras
+
+scoop install neovim
+
+Copy-Item .\.vimrc C:\Users\DBrags\AppData\Local\nvim\init.vim
+
