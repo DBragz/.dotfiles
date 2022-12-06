e903c6f4d05e582472f34020da210be86ec49264
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2022-12-06 23:43:16 +0000

    Created Linux install script

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+# Name: install.sh
+#
+# Author: Daniel Ribeirinha-Braga
+#
+# Description: Initial installation script for my Linux environment.
+#
+
+init () {
+  echo "Running Linux Install Script!";
+}
+
+init
+
