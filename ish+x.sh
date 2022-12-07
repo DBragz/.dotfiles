6877f7bc93bae29b45cb441cc794c5acd6592dfd
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2022-12-07 22:11:42 +0000

    Added file headers to ish-x.sh and ish+x.sh

--- ish+x.sh
+++ ish+x.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+#
+# Name: ish+x.sh
+#
+# Author: Daniel Ribeirinha-Braga
+#
+# Description: Adds execution permissions to install.sh.
+#
 
 chmod u+x install.sh
+
 ./install.sh
+
--- ish-x.sh
+++ ish-x.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+#
+# Name: ish-x.sh
+#
+# Author: Daniel Ribeirinha-Braga
+#
+# Description: Removes execution permissions from install.sh
+#
 
 chmod u-x install.sh
 
