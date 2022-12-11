db6eae28e21bcb551347a2b7304680bf6809cc89
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2022-12-11 17:02:13 +0000

    Added commit and push alias

--- os/alpine.sh
+++ os/alpine.sh
@@ -11,6 +11,9 @@ echo 'Running Alpine Install Script!'
 
 echo 'alias r='$HOME/.dotfiles/ish+x.sh'' > $HOME/.profile
 echo 'alias u='$HOME/.dotfiles/ish-x.sh'' >> $HOME/.profile
-echo 'alias g="git status"' >> $HOME/.profile
 echo 'alias l="ls -la"' >> $HOME/.profile
+echo 'alias g="git status"' >> $HOME/.profile
+echo 'alias a="git add ."' >> $HOME/.profile
+echo 'alias p="git push origin"' >> $HOME/.profile
+
 
