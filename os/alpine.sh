a6308a7cfa0525f62b8c2c4896a1b71e02b05abc
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2022-12-11 04:53:29 +0000

    Changed .ashrc to .profile

--- os/alpine.sh
+++ os/alpine.sh
@@ -3,14 +3,14 @@
 # Name: alpine.sh
 #
 # Author: Daniel Ribeirinha-Braga
-#
+ý
 # Description: Installation script for my alpine environment.
 #
 
 echo 'Running Alpine Install Script!'
 
-echo 'alias r='$HOME/.dotfiles/ish+x.sh'' > $HOME/.ashrc
-echo 'alias u='$HOME/.dotfiles/ish-x.sh'' >> $HOME/.ashrc
-echo 'alias g="git status"' >> $HOME/.ashrc
-echo 'alias l="ls -la"' >> $HOME/.ashrc
+echo 'alias r='$HOME/.dotfiles/ish+x.sh'' > $HOME/.profile
+echo 'alias u='$HOME/.dotfiles/ish-x.sh'' >> $HOME/.profile
+echo 'alias g="git status"' >> $HOME/.profile
+echo 'alias l="ls -la"' >> $HOME/.profile
 
