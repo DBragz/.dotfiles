4d0d33b2d40e5539296ac6d1538946bd63220c63
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2022-12-12 00:22:51 +0000

    Added vim install

--- os/alpine.sh
+++ os/alpine.sh
@@ -10,4 +10,12 @@
 echo 'Running Alpine Install Script!'
 
 cp $HOME/.dotfiles/configs/.profile $HOME/
+cp $HOME/.dotfiles/configs/.vimrc $HOME/
+
+if ! command -v vim &> /dev/null
+then
+    echo 'ERROR: vim could not be found'
+    echo 'Installing vim'
+    apk add vim
+fi
 
