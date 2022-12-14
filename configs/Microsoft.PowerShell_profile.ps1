18153ba58eee25e7fa63bfa558af2cf8348f7781
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2022-12-14 02:39:18 -0500

    Addeed header to my powershell profile.

--- configs/Microsoft.PowerShell_profile.ps1
+++ configs/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,9 @@
+<# .SYNOPSIS
+	Windows Profile Settings
+.DESCRIPTION
+  My default Windows PowerShell settings.
+.NOTES
+	Author: Daniel Ribeirinha-Braga - dmrbraga@gmail.com
+#>
+
 Invoke-Expression (&starship init powershell)
