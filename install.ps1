a8d45627c33bdc462c1b9bdf885138fbc00359d2
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2022-12-17 16:45:06 -0500

    Fixed folder reference.

--- install.ps1
+++ install.ps1
@@ -6,6 +6,17 @@
 	Author: Daniel Ribeirinha-Braga - dmrbraga@gmail.com
 #>
 
+New-Item -Type Directory -Force $env:LOCALAPPDATA\clink
+
+Copy-Item $env:HOMEPATH\.dotfiles\configs\starship.lua $env:LOCALAPPDATA\clink\
+
+Copy-Item $env:HOMEPATH\.dotfiles\configs\Microsoft.PowerShell_profile.ps1 $PROFILE
+
+New-Item -Type Directory -Force $env:LOCALAPPDATA\nvim\lua\
+
+Copy-Item $env:HOMEPATH\.dotfiles\configs\init.lua $env:LOCALAPPDATA\nvim\
+Copy-Item $env:HOMEPATH\.dotfiles\configs\remap.lua $env:LOCALAPPDATA\nvim\lua\
+
 Invoke-WebRequest -useb chocolatey.org/install.ps1 | Invoke-Expression
 
 Invoke-WebRequest -useb get.scoop.sh | Invoke-Expression
@@ -16,15 +27,4 @@ scoop bucket add extras
 winget install -e --id Microsoft.PowerShell
 
 scoop install clink starship fzf neovim
-
-New-Item -Type Directory $env:LOCALAPPDATA\clink
-
-Copy-Item $env:HOMEPATH\.dotfiles\configs\starship.lua $env:LOCALAPPDATA\clink\
-
-Copy-Item $env:HOMEPATH\.dotfiles\Microsoft.PowerShell_profile.ps1 $PROFILE
-
-New-Item -Type Directory $env:LOCALAPPDATA\nvim\lua\
-
-Copy-Item $env:HOMEPATH\.dotfiles\configs\init.lua $env:LOCALAPPDATA\nvim\
-Copy-Item $env:HOMEPATH\.dotfiles\configs\remap.lua $env:LOCALAPPDATA\nvim\lua\
 
