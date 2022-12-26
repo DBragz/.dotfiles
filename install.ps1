044211430acee11a47eee6989565b2d83b640d85
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2022-12-26 02:08:44 -0500

    Fixed telescope plugin

--- install.ps1
+++ install.ps1
@@ -8,7 +8,7 @@
 
 New-Item -Type Directory -Force $env:LOCALAPPDATA\clink
 New-Item -Type Directory -Force $env:LOCALAPPDATA\nvim\lua\
-New-Item -Type Directory -Force $env:LOCALAPPDATA\nvim\lua\after\
+New-Item -Type Directory -Force $env:LOCALAPPDATA\nvim\lua\after\plugin\
 New-Item -Type File -Force $PROFILE
 
 Copy-Item $env:HOMEPATH\.dotfiles\configs\Microsoft.PowerShell_profile.ps1 $PROFILE
@@ -18,9 +18,9 @@ Copy-Item $env:HOMEPATH\.dotfiles\configs\init.lua $env:LOCALAPPDATA\nvim\
 Copy-Item $env:HOMEPATH\.dotfiles\configs\options.lua $env:LOCALAPPDATA\nvim\lua\
 Copy-Item $env:HOMEPATH\.dotfiles\configs\remap.lua $env:LOCALAPPDATA\nvim\lua\
 Copy-Item $env:HOMEPATH\.dotfiles\configs\plugins.lua $env:LOCALAPPDATA\nvim\lua\
-Copy-Item $env:HOMEPATH\.dotfiles\configs\telescope.lua $env:LOCALAPPDATA\nvim\lua\after\
+Copy-Item $env:HOMEPATH\.dotfiles\configs\telescope.lua $env:LOCALAPPDATA\nvim\after\plugin
 
-git clone https://github.com/wbthomason/packer.nvim $env:LOCALAPPDATA\nvim-data\site\pack\packer\start\packer.nvim
+git clone https://github.com/wbthomason/packer.nvim "$env:LOCALAPPDATA\nvim-data\site\pack\packer\start\packer.nvim"
 
 choco install clink-maintained -y --force
 
@@ -68,15 +68,11 @@ if(-not (Get-Command fzf -errorAction silentlyContinue)){
 	scoop install fzf
 }
 
-if(-not (Get-Command neovim -errorAction silentlyContinue)){
+if(-not (Get-Command nvim -errorAction silentlyContinue)){
 	Write-Host "Error: neovim could not be found"
 	Write-Host "Installing neovim"
 	scoop install neovim
 }
-
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::Machine)
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::Process)
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::User)
 
 oh-my-posh font install Meslo
 
