ce8dca5b6af82ddcdffe82e93bbbb816235c8c6b
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2022-12-26 01:54:55 -0500

    Fixing plugins

--- configs/plugins.lua
+++ configs/plugins.lua
@@ -1,16 +1,12 @@
 vim.cmd("echo \"Oi from plugins!\"")
 
 return require('packer').startup {
-	function(use)
-		use 'wbthomason/packer.nvim'
-		use {
-			'https://github.com/nvim-lua/plenary.nvim.git',
-			as = 'nvim-lua/planary.nvim'
-		}
-		use { 
-			'https://github.com/nvim-telescope/telescope.nvim.git',
-			as = 'nvim-telescope/telescope.nvim', tag = '0.1.0',
-		}
-	end
+  function(use)
+    use 'wbthomason/packer.nvim'
+    use {
+      'nvim-telescope/telescope.nvim', tag = '0.1.0',
+      requires = { {'nvim-lua/plenary.nvim'} }
+    }
+  end
 }
 
