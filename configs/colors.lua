9763bfe1051b56ec2dbaf6ca64f0703b66584ff3
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2022-12-27 13:12:45 -0500

    Removed unneeded colorscheme

--- configs/colors.lua
+++ configs/colors.lua
@@ -1,5 +1,3 @@
-vim.cmd.colorscheme('poimandres')
-
 vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
 
--- configs/plugins.lua
+++ configs/plugins.lua
@@ -7,9 +7,6 @@ return require('packer').startup {
       'nvim-telescope/telescope.nvim', tag = '0.1.0',
       requires = { {'nvim-lua/plenary.nvim'} }
     }
-    use { 
-      'olivercederborg/poimandres.nvim'
-    }
   end
 }
 
