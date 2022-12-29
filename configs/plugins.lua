4a75634d3199faa13a1455e88a0854eea16fc685
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2022-12-28 20:51:33 -0500

    Added neovim icons

--- configs/plugins.lua
+++ configs/plugins.lua
@@ -8,6 +8,12 @@ return require("packer").startup {
       requires = { {"nvim-lua/plenary.nvim"} }
     }
     use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    use({
+      "kyazdani42/nvim-tree.lua",
+      requires = {
+        "kyazdani42/nvim-web-devicons", -- optional, for file icons
+      },
+    })
   end
 }
 
