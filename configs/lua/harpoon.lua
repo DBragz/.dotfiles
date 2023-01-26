c6e5a5b141c9adfd66084fef03636660b53f0b37
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2023-01-25 22:09:30 -0700

    Changed iterating through files with leader key

--- configs/lua/harpoon.lua
+++ configs/lua/harpoon.lua
@@ -10,8 +10,8 @@ local ui = require("harpoon.ui")
 vim.keymap.set("n", "<C-a>", mark.add_file)
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
-vim.keymap.set("n", "<C-z>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<C-x>", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<C-c>", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<C-v>", function() ui.nav_file(4) end)
+vim.keymap.set("n", "<leader>1", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<leader>2", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<leader>3", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<leader>4", function() ui.nav_file(4) end)
 
