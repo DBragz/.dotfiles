eb5ac2a64166e4e2caf3ebcd0c12deef9a8216d4
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2023-01-30 01:17:38 +0000

    Added initial aliasrs to .zshrc and PowerShell_profile.ps1

--- configs/profiles/.zshrc
+++ configs/profiles/.zshrc
@@ -6,6 +6,18 @@
 # Description: Profile file for Oh My Zsh shell.
 #
 
+alias i=$HOME/.dotfiles/install.sh
+alias u=$HOME/.dotfiles/uninstall.sh
+alias l="ls -la"
+alias e=exit
+alias c=clear
+alias gs="git status"
+alias ga="git add ."
+alias gd="git diff"
+alias gc="git commit -m $1"
+alias gpush="git push origin"
+alias gpull="git pull"
+
 eval "$(starship init zsh)"
 1
 export PATH="/usr/local/opt/openjdk/bin:$PATH"
--- configs/profiles/Microsoft.PowerShell_profile.ps1
+++ configs/profiles/Microsoft.PowerShell_profile.ps1
@@ -7,5 +7,17 @@
   Name: Microsoft.PowerShell_profile.ps1
 #>
 
+Set-Alias -Name i -Value $env:HOMEPATH/.dotfiles/install.ps1
+Set-Alias -Name u -Value $env:HOMEPATH/.dotfiles/uninstall.ps1
+#Set-Alias -Name l -Value ls -la
+#Set-Alias -Name e -Value exit
+#Set-Alias -Name c -Value clear
+Set-Alias -Name gs -Value git status
+Set-Alias -Name ga -Value git add .
+Set-Alias -Name gd -Value git diff
+Set-Alias -Name gc -Value git commit -m $args[1]
+Set-Alias -Name gpush -Value git push origin
+Set-Alias -Name gpull -Value git pull
+
 Invoke-Expression (&starship init powershell)
 
