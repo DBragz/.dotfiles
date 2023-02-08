956b13cca7315d4d54dca637ede929be40004649
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2023-02-07 19:06:48 -0500

    Create termux.sh

---
+++ os/termux.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+#
+# Name: termux.sh
+#
+# Description: Installation script for my termux environment.
+#
+# Author: Daniel Ribeirinha-Braga
+#
+
+cp $HOME/.dotfiles/configs/profiles/.profile $HOME/
+cp $HOME/.dotfiles/configs/profiles/.vimrc $HOME/
+
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+  curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+cd ~/.vim/bundle && \
+  git clone --depth=1 https://github.com/vim-syntastic/syntastic.git
+
+if ! command -v man &> /dev/null
+then
+  echo "Error: man could not be found"
+  echo "Installing man"
+  pkg install man
+fi
+
+if ! command -v vim &> /dev/null
+then
+  echo "Error: vim could not be found"
+  echo "Installing VIM - Vi IMproved"
+  pkg install vim
+fi
+
+echo "Completed Termux install script!"
