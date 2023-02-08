4f9bc6c148843a86a4ccbb96aa162b3f98c7bcaf
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2023-02-07 19:09:51 -0500

    Create termux_uninstall.sh

---
+++ os/termux_uninstall.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# Name: termux_uninstall.sh
+#
+# Description: Uninstallation script for my Termux environment.
+#
+# Author: Daniel Ribeirinha-Braga
+#
+
+pkg uninstall man tmux fzf vim
+
+echo "Completed Termux uninstall script!"
