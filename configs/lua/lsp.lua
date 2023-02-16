96c671b42841854c7d6162bf1d66d8e57cff0190
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2023-02-16 15:03:28 -0600

    Added hover code action and rename

--- configs/lua/lsp.lua
+++ configs/lua/lsp.lua
@@ -6,6 +6,14 @@
 
 vim.keymap.set("n", "gd", vim.lsp.buf.definition)
 vim.keymap.set("n", "K", vim.lsp.buf.hover)
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
+
+--vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol)
+--vim.keymap.set("n", "<leader>vd", vim.lsp.buf.open_float)
+--vim.keymap.set("n", "[d", vim.lsp.buf.goto_next)
+--vim.keymap.set("n", "[d", vim.lsp.buf.goto_prev)
+--vim.keymap.set("n", "<C-h>", vim.lsp.buf.signature_help)
 
 local lsp = require("lsp-zero")
 
--- configs/lua/tive.lua
+++ configs/lua/tive.lua
@@ -8,3 +8,4 @@ vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 vim.keymap.set("n", "<leader>ga", vim.cmd.Git.Add)
 
 --vim.keymap.set("n", "", vim.cmd.)
+
