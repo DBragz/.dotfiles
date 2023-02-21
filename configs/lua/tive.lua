8eb4ef92510018646bc33bf2a4ec2486303d6bc1
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2023-02-20 20:44:20 -0600

    Working on git commit

--- configs/lua/tive.lua
+++ configs/lua/tive.lua
@@ -6,5 +6,8 @@
 
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 vim.keymap.set("n", "<leader>ga", ":G add .<CR>")
+vim.keymap.set("n", "<leader>gm", function() print(_G[1]) end)
+vim.keymap.set("n", "<leader>gma", ":G commit -m ")
+--vim.keymap.set("n", "<leader>gmai", function(":G commit -m " .. args[1] end)
 vim.keymap.set("n", "<leader>gp", ":G push<CR>")
 
