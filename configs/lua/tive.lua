1e6f74770766d4b9f7391167ac6730adbc129c20
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2023-03-07 21:15:21 +0000

    Updated commit in Neovim

--- configs/lua/tive.lua
+++ configs/lua/tive.lua
@@ -6,7 +6,7 @@
 
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 vim.keymap.set("n", "<leader>ga", ":G add .<CR>")
-vim.keymap.set("n", "<leader>gc", ":G commit -m ")
+vim.keymap.set("n", "<leader>gct", ":G commit -m ")
 --vim.keymap.set("n", "<leader>gmai", function(":G commit -m " .. args[1] end)
 vim.keymap.set("n", "<leader>gph", ":G push<CR>")
 vim.keymap.set("n", "<leader>gpl", ":G pull<CR>")
