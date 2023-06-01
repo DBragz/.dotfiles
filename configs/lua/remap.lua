1f3d4361654be604abcf668470404437c0badc16
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2023-06-01 09:27:01 -0400

    Update remap.lua

--- configs/lua/remap.lua
+++ configs/lua/remap.lua
@@ -4,15 +4,14 @@
 -- Author: Daniel Ribeirinha-Braga
 --
 
-vim.g.mapleader = " "
 vim.g.maplocalleader = "g"
+vim.g.mapleader = " "
 
-vim.keymap.set("n", "<leader>e", vim.cmd.E)
 vim.keymap.set("n", "<localleader>e", vim.cmd.E)
+vim.keymap.set("t", "<leader>e", "<C-\\><C-n>")
 vim.keymap.set("n", "<leader>w", ":w<CR>")
 vim.keymap.set("n", "<leader>ps", ":PackerSync<CR>")
 vim.keymap.set("n", "<leader>tsu", ":TSUpdate<CR>")
 vim.keymap.set("n", "<leader>so", ":so<CR>")
 vim.keymap.set("n", "<C-_>", function() print("control /") end)
 vim.keymap.set("n", "<leader>p", function() print(_G) end)
-
