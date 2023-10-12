4143dfca0bf03373e91af5363c89b9c1ce54dd79
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2023-10-12 00:16:42 -0400

    Fixed which key remaping.

--- configs/lua/remap.lua
+++ configs/lua/remap.lua
@@ -7,7 +7,7 @@
 vim.g.maplocalleader = "g"
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "<localleader>e", vim.cmd.E)
+vim.keymap.set("n", "<localleader>E", ":E<CR>")
 vim.keymap.set("t", "<leader>e", "<C-\\><C-n>")
 vim.keymap.set("n", "<leader>so", ":so<CR>")
 vim.keymap.set("n", "<leader>w", ":w<CR>")
