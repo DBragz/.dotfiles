b258b16b3542ef3e389ce0e053bd65b290a5fc90
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2023-10-17 23:28:29 -0600

    Added treesitter configurations.

--- configs/lua/treesitter.lua
+++ configs/lua/treesitter.lua
@@ -4,21 +4,20 @@
 -- Author:      Daniel Ribeirinha-Braga
 --
 
---require("nvim-treesitter.configs").setup {
---  ensure_installed = {
---    "bash",
---    "java",
---    "javascript",
---    "lua",
---    "python",
---    "solidity",
---    "typescript",
---  },
---  sync_install = false,
---  auto_install = true,
---  highlight = {
---    enable = true,
---    additional_vim_regex_highlighting = false,
---  },
---}
+require("nvim-treesitter.configs").setup {
+  ensure_installed = {
+    "java",
+    "javascript",
+    "lua",
+    "python",
+    "solidity",
+    "typescript",
+  },
+  sync_install = false,
+  auto_install = true,
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+}
 
