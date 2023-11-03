f9a19246605a7cdc0ccc273662386eb64f528a59
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2023-11-03 13:25:12 -0600

    Added project search.

--- configs/lua/telescope.lua
+++ configs/lua/telescope.lua
@@ -10,3 +10,14 @@ vim.keymap.set("n", "<leader>tg", ":Telescope git_files<CR>")
 vim.keymap.set("n", "<leader>td", ":Telescope diagnostics<CR>")
 vim.keymap.set("n", "<leader>tl", ":Telescope lsp_references<CR>")
 
+HOME = ""
+
+if os.getenv("HOME") ~= nil
+  then
+    HOME = tostring(os.getenv("HOME"))
+    vim.keymap.set('n', '<leader>tgr', ":Telescope grep_string<CR>")
+  else
+    HOME = tostring(os.getenv("HOMEPATH"))
+    print("No grep_string :(")
+  end
+
