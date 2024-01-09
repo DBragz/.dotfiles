4b0f01bc2ebd8ac34236cac25351435761806c64
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2024-01-08 23:35:17 -0500

    Added cycling hotkeys.

--- configs/lua/plugins.lua
+++ configs/lua/plugins.lua
@@ -44,7 +44,16 @@ return require("packer").startup {
         {'L3MON4D3/LuaSnip'},
       }
     }
-    use 'Exafunction/codeium.vim'
+    use {
+      'Exafunction/codeium.vim',
+      config = function ()
+        -- Change '<C-g>' here to any keycode you like.
+        vim.keymap.set('i', '<C-g>', function () return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
+        vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
+        vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true, silent = true })
+        vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
+      end
+    }
   end
 }
 
