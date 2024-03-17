6fdbeb6ba1937e907f482765dd2ca5020a4b7d69
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2024-03-17 13:20:59 -0400

    Updated fzf and bat.

--- install.ps1
+++ install.ps1
@@ -45,106 +45,28 @@ if (-not (Test-Path $env:LOCALAPPDATA\clink -PathType Container)) {
 
 Copy-Item $env:HOMEPATH\.dotfiles\configs\lua\starship.lua $env:LOCALAPPDATA\clink\
 
-if (-not (Get-Command pwsh -errorAction SilentlyContinue)) {
-  Write-Host "Error: PowerShell 7 not found"
-  Write-Host "Installing Microsoft.PowerShell"
-  winget install -e --id Microsoft.PowerShell
-}
-
-if (-not (Test-Path $env:USERPROFILE\OneDrive\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 -PathType Leaf)) {
-  New-Item -Type File $env:USERPROFILE\OneDrive\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 -Force
-}
-
-Copy-Item $env:HOMEPATH\.dotfiles\configs\profiles\Microsoft.PowerShell_profile.ps1 $env:USERPROFILE\OneDrive\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
-
-if (-not (Test-Path $env:USERPROFILE\OneDrive\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 -PathType Leaf)) {
-  New-Item -Type File $env:USERPROFILE\OneDrive\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 -Force
-}
-
-Copy-Item $env:HOMEPATH\.dotfiles\configs\profiles\Microsoft.PowerShell_profile.ps1 $env:USERPROFILE\OneDrive\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
-
-if (-not (Get-Command wt -errorAction SilentlyContinue)) {
-  Write-Host "Error: Windows Terminal could not be found"
-  Write-Host "Installing windows-terminal"
-  scoop install windows-terminal
-}
-
-Copy-Item $env:HOMEPATH\.dotfiles\configs\json\terminal.json $env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json 
-
 if (-not (Get-Command oh-my-posh -errorAction SilentlyContinue)) {
   Write-Host "Error: Oh My Posh could not be found"
   Write-Host "Installing oh-my-posh"
   winget install JanDeDobbeleer.OhMyPosh -s winget
 }
 
-if ((Get-ChildItem C:\Windows\Fonts\ | Out-String -Stream | Select-String -Pattern "Meslo").Count -eq 0) {
-  if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    Start-Process -Wait powershell -Verb runAs "oh-my-posh font install Meslo"
-  } 
+if (-not (Get-Command pwsh -errorAction SilentlyContinue)) {
+  Write-Host "Error: PowerShell 7 not found"
+  Write-Host "Installing Microsoft.PowerShell"
+  winget install -e --id Microsoft.PowerShell
 }
 
 if (-not (Get-Command fzf -errorAction silentlyContinue)) {
   Write-Host "Error: Command-line fuzzy finder could not be found"
   Write-Host "Installing fzf"
-  scoop install fzf
+  winget install -e --id junegunn.fzf
 }
 
 if (-not (Get-Command bat -errorAction silentlyContinue)) {
   Write-Host "Error: Bat could not be found"
   Write-Host "Installing bat"
-  scoop install bat
-}
-
-if (-not (Get-Command nvim -errorAction silentlyContinue)) {
-  Write-Host "Error: Neovim could not be found"
-  Write-Host "Installing neovim"
-  winget install -e --id Neovim.Neovim
-}
-
-if (-not (Test-Path $env:LOCALAPPDATA\nvim-data\site\pack\packer\start\packer.nvim -PathType Container)) {
-  git clone https://github.com/wbthomason/packer.nvim "$env:LOCALAPPDATA\nvim-data\site\pack\packer\start\packer.nvim"
-}
-
-if (-not (Test-Path $env:LOCALAPPDATA\nvim\lua\ -PathType Container)) {
-  New-Item -Type Directory $env:LOCALAPPDATA\nvim\lua\
-}
-
-if (-not (Test-Path $env:LOCALAPPDATA\nvim\after\plugin\ -PathType Container)) {
-  New-Item -Type Directory $env:LOCALAPPDATA\nvim\after\plugin\
-}
-
-Copy-Item $env:HOMEPATH\.dotfiles\configs\lua\init.lua $env:LOCALAPPDATA\nvim\
-Copy-Item $env:HOMEPATH\.dotfiles\configs\lua\options.lua $env:LOCALAPPDATA\nvim\lua\
-Copy-Item $env:HOMEPATH\.dotfiles\configs\lua\plugins.lua $env:LOCALAPPDATA\nvim\lua\
-Copy-Item $env:HOMEPATH\.dotfiles\configs\lua\colors.lua $env:LOCALAPPDATA\nvim\after\plugin\
-Copy-Item $env:HOMEPATH\.dotfiles\configs\lua\remap.lua $env:LOCALAPPDATA\nvim\after\plugin\
-Copy-Item $env:HOMEPATH\.dotfiles\configs\lua\tree.lua $env:LOCALAPPDATA\nvim\after\plugin\
-Copy-Item $env:HOMEPATH\.dotfiles\configs\lua\tive.lua $env:LOCALAPPDATA\nvim\after\plugin\
-Copy-Item $env:HOMEPATH\.dotfiles\configs\lua\treesitter.lua $env:LOCALAPPDATA\nvim\after\plugin\
-Copy-Item $env:HOMEPATH\.dotfiles\configs\lua\undotree.lua $env:LOCALAPPDATA\nvim\after\plugin\
-Copy-Item $env:HOMEPATH\.dotfiles\configs\lua\server.lua $env:LOCALAPPDATA\nvim\after\plugin\
-Copy-Item $env:HOMEPATH\.dotfiles\configs\lua\telescope.lua $env:LOCALAPPDATA\nvim\after\plugin\
-Copy-Item $env:HOMEPATH\.dotfiles\configs\lua\harpoon.lua $env:LOCALAPPDATA\nvim\after\plugin\
-
-if (-not (Get-Command code -errorAction silentlyContinue)) {
-  Write-Host "Error: Visual Studio Code could not be found"
-  Write-Host "Installing code"
-  winget install -e --id Microsoft.VisualStudioCode
-}
-
-if (-not (Test-Path $env:HOMEPATH\AppData\Roaming\Code\User\ -PathType Container)) {
-  New-Item -Type Directory -Path $env:HOMEPATH\AppData\Roaming\Code\User\
-}
-
-Copy-Item $env:HOMEPATH\.dotfiles\configs\json\vscode.json $env:HOMEPATH\AppData\Roaming\Code\User\settings.json 
-
-if (-not (Get-Command docker -errorAction silentlyContinue)) {
-  Write-Host "Error: Docker could not be found"
-  Write-Host "Installing docker-desktop"
-  if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    Start-Process -Wait powershell -Verb runAs "choco install docker-desktop -y"
-  }
-  wsl --update
+  winget install -e --id sharkdp.bat
 }
 
 if (-not (Get-Command java -errorAction silentlyContinue)) {
@@ -179,6 +101,78 @@ if (-not (Get-Command gcc -errorAction silentlyContinue)) {
   Write-Host "Installing gcc"
   scoop install gcc
   scoop reset gcc
+}
+
+if (-not (Get-Command nvim -errorAction silentlyContinue)) {
+  Write-Host "Error: Neovim could not be found"
+  Write-Host "Installing neovim"
+  winget install -e --id Neovim.Neovim
+}
+
+if (-not (Test-Path $env:LOCALAPPDATA\nvim-data\site\pack\packer\start\packer.nvim -PathType Container)) {
+  git clone https://github.com/wbthomason/packer.nvim "$env:LOCALAPPDATA\nvim-data\site\pack\packer\start\packer.nvim"
+}
+
+if (-not (Test-Path $env:LOCALAPPDATA\nvim\lua\ -PathType Container)) {
+  New-Item -Type Directory $env:LOCALAPPDATA\nvim\lua\
+}
+
+if (-not (Test-Path $env:LOCALAPPDATA\nvim\after\plugin\ -PathType Container)) {
+  New-Item -Type Directory $env:LOCALAPPDATA\nvim\after\plugin\
+}
+
+Copy-Item $env:HOMEPATH\.dotfiles\configs\lua\init.lua $env:LOCALAPPDATA\nvim\
+Copy-Item $env:HOMEPATH\.dotfiles\configs\lua\options.lua $env:LOCALAPPDATA\nvim\lua\
+Copy-Item $env:HOMEPATH\.dotfiles\configs\lua\plugins.lua $env:LOCALAPPDATA\nvim\lua\
+Copy-Item $env:HOMEPATH\.dotfiles\configs\lua\colors.lua $env:LOCALAPPDATA\nvim\after\plugin\
+Copy-Item $env:HOMEPATH\.dotfiles\configs\lua\remap.lua $env:LOCALAPPDATA\nvim\after\plugin\
+Copy-Item $env:HOMEPATH\.dotfiles\configs\lua\tree.lua $env:LOCALAPPDATA\nvim\after\plugin\
+Copy-Item $env:HOMEPATH\.dotfiles\configs\lua\tive.lua $env:LOCALAPPDATA\nvim\after\plugin\
+Copy-Item $env:HOMEPATH\.dotfiles\configs\lua\treesitter.lua $env:LOCALAPPDATA\nvim\after\plugin\
+Copy-Item $env:HOMEPATH\.dotfiles\configs\lua\undotree.lua $env:LOCALAPPDATA\nvim\after\plugin\
+Copy-Item $env:HOMEPATH\.dotfiles\configs\lua\server.lua $env:LOCALAPPDATA\nvim\after\plugin\
+Copy-Item $env:HOMEPATH\.dotfiles\configs\lua\telescope.lua $env:LOCALAPPDATA\nvim\after\plugin\
+Copy-Item $env:HOMEPATH\.dotfiles\configs\lua\harpoon.lua $env:LOCALAPPDATA\nvim\after\plugin\
+
+if (-not (Test-Path $env:USERPROFILE\OneDrive\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 -PathType Leaf)) {
+  New-Item -Type File $env:USERPROFILE\OneDrive\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 -Force
+}
+
+Copy-Item $env:HOMEPATH\.dotfiles\configs\profiles\Microsoft.PowerShell_profile.ps1 $env:USERPROFILE\OneDrive\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+
+if (-not (Test-Path $env:USERPROFILE\OneDrive\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 -PathType Leaf)) {
+  New-Item -Type File $env:USERPROFILE\OneDrive\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 -Force
+}
+
+Copy-Item $env:HOMEPATH\.dotfiles\configs\profiles\Microsoft.PowerShell_profile.ps1 $env:USERPROFILE\OneDrive\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
+
+Copy-Item $env:HOMEPATH\.dotfiles\configs\json\terminal.json $env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json 
+
+if ((Get-ChildItem C:\Windows\Fonts\ | Out-String -Stream | Select-String -Pattern "Meslo").Count -eq 0) {
+  if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Start-Process -Wait powershell -Verb runAs "oh-my-posh font install Meslo"
+  } 
+}
+
+if (-not (Get-Command code -errorAction silentlyContinue)) {
+  Write-Host "Error: Visual Studio Code could not be found"
+  Write-Host "Installing code"
+  winget install -e --id Microsoft.VisualStudioCode
+}
+
+if (-not (Test-Path $env:HOMEPATH\AppData\Roaming\Code\User\ -PathType Container)) {
+  New-Item -Type Directory -Path $env:HOMEPATH\AppData\Roaming\Code\User\
+}
+
+Copy-Item $env:HOMEPATH\.dotfiles\configs\json\vscode.json $env:HOMEPATH\AppData\Roaming\Code\User\settings.json 
+
+if (-not (Get-Command docker -errorAction silentlyContinue)) {
+  Write-Host "Error: Docker could not be found"
+  Write-Host "Installing docker-desktop"
+  if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Start-Process -Wait powershell -Verb runAs "choco install docker-desktop -y"
+  }
+  wsl --update
 }
 
 Write-Host "Completed Windows 11 install script"
