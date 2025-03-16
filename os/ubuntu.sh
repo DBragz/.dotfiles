6cbc7b5bf66266dd1ba824088908604c89deb7bc
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2025-03-15 22:20:22 -0400

    Switching bat for cat.

--- os/ubuntu.sh
+++ os/ubuntu.sh
@@ -11,12 +11,5 @@ cp $HOME/.dotfiles/configs/profiles/.bashrc $HOME
 cp $HOME/.dotfiles/configs/profiles/.tmux.conf $HOME
 cp $HOME/.dotfiles/configs/profiles/.vimrc $HOME/
 
-if ! command -v bat &> /dev/null
-then
-  echo "Error: Bat could not be found"
-  echo "Installing bat"
-  apt install bat
-fi
-
 echo "Completed Ubuntu Linux install script!"
 
