eaf2d5513f368d332978759bb41d6453f22d17b0
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2025-03-15 22:14:45 -0400

    Adding bat installation.

--- os/ubuntu.sh
+++ os/ubuntu.sh
@@ -11,5 +11,12 @@ cp $HOME/.dotfiles/configs/profiles/.bashrc $HOME
 cp $HOME/.dotfiles/configs/profiles/.tmux.conf $HOME
 cp $HOME/.dotfiles/configs/profiles/.vimrc $HOME/
 
+if ! command -v bat &> /dev/null
+then
+  echo "Error: Bat could not be found"
+  echo "Installing bat"
+  brew install bat
+fi
+
 echo "Completed Ubuntu Linux install script!"
 
