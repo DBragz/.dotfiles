e7a71aaf4f075e99281a3353bfab349c73201078
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2025-03-15 22:02:19 -0400

    Created Ubuntu script.

---
+++ os/ubuntu.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+# Name: ubuntu.sh
+#
+# Author: Daniel Ribeirinha-Braga
+#
+# Description: Installation script for my Ubuntu Linux environment.
+#
+
+cp $HOME/.dotfiles/configs/profiles/.tmux.conf $HOME
+cp $HOME/.dotfiles/configs/profiles/.vimrc $HOME/
+
+echo "Completed Ubuntu Linux install script!"
+
